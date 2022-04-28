475e5458b144d8d67bb280d5ab324127788f455a
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-04-28 10:22:51 -0700

    some changes

---
+++ scripts/update_larsoft.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+#---------------------Directory---------------------#
+# this handy piece of code determines the relative
+# directory that this script is in.
+SOURCE="${BASH_SOURCE[0]}"
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do 
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative 
+  # to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+done
+LARSOFT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )/../"
+
+#---------------------Installation Directory--------#
+INSTALL_DIRECTORY=$LARSOFT_DIR/larsoft
+mkdir -p $INSTALL_DIRECTORY
+cd $INSTALL_DIRECTORY
+
+#--------------------Versioning---------------------#
+# specify the version of the larsoft packages.
+LARSOFT_VERSION=v09_31_00
+QUALS=e20:prof
+
+#--------------------Setup LArSoft------------------#
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup larsoft $LARSOFT_VERSION -q $QUALS
+setup ninja
+cd $INSTALL_DIRECTORY
+source localProducts*/setup
+mrbslp
+
+#--------------------Custom updating code-----------#
+# here we specify how we want to update our custom
+# larsoft, e.g. by pulling some information into
+# a custom module:
+#   $ cd $MRB_SOURCE/larsoft/MyCustomModule"
+#   $ git pull
+
+#--------------------Rebuild------------------------#
+ninja -C $MRB_BUILDDIR -j 32 install
+
+cd $LARSOFT_DIR
